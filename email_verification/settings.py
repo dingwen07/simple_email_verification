8ad027d046650cdd38f9802c159db3ce297cf9cd
Dingwen Wang <wangdingwen07@gmail.com>
2021-10-19 22:06:24 -0400

    Update settings.py

--- email_verification/settings.py
+++ email_verification/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import decouple
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4uz2oi7iy&!c#n($@gn(!5ev3ckp@v%v18*m#1fvxokg3kds#q'
+SECRET_KEY = decouple.config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
